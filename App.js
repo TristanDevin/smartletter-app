d6929b1a461037ebe2f3d994ea1933b325f404c7
TristanDevin <tristan.devin@insa-lyon.fr>
2023-10-22 22:43:46 +0200

    feat: :art: remove placeholder text

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { View, Text, Image } from "react-native";
-import { NativeBaseProvider, Box } from "native-base";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons"; // Import Ionicons from Expo package
 
@@ -15,119 +14,138 @@ export default function App() {
   };
 
   return (
-    <NativeBaseProvider>
-      <View style={{ flex: 1, backgroundColor: "black" }}>
-        {/* Top Bar */}
-        <View
-          style={{
-            height: 50,
-            backgroundColor: "grey",
-            justifyContent: "flex-start",
-            alignItems: "flex-start",
-          }}
-        >
-          <Text style={{ color: "white" }}>SmartLetter</Text>
-          <Text style={{ color: "white" }}>Bonne journée, Rosalie</Text>
-        </View>
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      {/* Top Bar */}
+      <View
+        style={{
+          height: 50,
+          backgroundColor: "grey",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        }}
+      >
+        <Text style={{ color: "white" }}>SmartLetter</Text>
+        <Text style={{ color: "white" }}>Bonne journée, Rosalie</Text>
+      </View>
 
-        {/* Main Content */}
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          <View style={{ width: "100%", marginBottom: 20 }}>
-            <View
-              style={{
-                backgroundColor: "rgba(255, 255, 255, 0.5)",
-                paddingTop: 40,
-                paddingBottom: 40,
-                shadowColor: "#000",
-                shadowOffset: { width: 2, height: 2 },
-                shadowOpacity: 0.5,
-                shadowRadius: 4,
-                marginBottom: 10,
-                position: "relative", // Ensure the button is positioned relative to this parent
-              }}
-            >
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-              <Text style={{ color: 'white', textAlign: 'center', flex: 1 }}>Content taking up the full width</Text>
-              <TouchableOpacity onPress={handleButtonClick}>
-                <Ionicons name="arrow-forward-outline" size={24} color="white" />
-              </TouchableOpacity>
-            </View>
-          </View>
-            <View
-              style={{
-                backgroundColor: "rgba(255, 255, 255, 0.5)",
-                paddingTop: 40,
-                paddingBottom: 40,
-                shadowColor: "#000",
-                shadowOffset: { width: 2, height: 2 },
-                shadowOpacity: 0.5,
-                shadowRadius: 4,
-                marginBottom: 10,
-                position: "relative", // Ensure the button is positioned relative to this parent
-              }}
-            >
-             <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-              <Text style={{ color: 'white', textAlign: 'center', flex: 1 }}>Content taking up the full width</Text>
-              <TouchableOpacity onPress={handleButtonClick}>
-                <Ionicons name="arrow-forward-outline" size={24} color="white" />
-              </TouchableOpacity>
-            </View>
-          </View>
-          </View>
-        </View>
-
-        {/* Footer Bar */}
-        <View
-          style={{
-            height: 50,
-            backgroundColor: "grey",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingHorizontal: 20,
-          }}
-        >
-          <Text style={{ color: "white" }}>Hello world</Text>
-          <TouchableOpacity onPress={handleButtonClick}>
-            <Image
-              source={require("./assets/icons/gear.png")}
-              style={{ width: 25, height: 25 }}
-            />
-          </TouchableOpacity>
-        </View>
-        {/* Popup */}
-        {popupVisible && (
+      {/* Main Content */}
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View style={{ width: "100%", marginBottom: 20 }}>
           <View
             style={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              paddingTop: 40,
+              paddingBottom: 40,
+              shadowColor: "#000",
+              shadowOffset: { width: 2, height: 2 },
+              shadowOpacity: 0.5,
+              shadowRadius: 4,
+              marginBottom: 10,
+              position: "relative", // Ensure the button is positioned relative to this parent
             }}
           >
             <View
               style={{
-                backgroundColor: "white",
-                padding: 20,
-                borderRadius: 8,
-                shadowColor: "#000",
-                shadowOffset: { width: 2, height: 2 },
-                shadowOpacity: 0.5,
-                shadowRadius: 4,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
               }}
             >
-              <Text>Clicked!</Text>
+              <Text style={{ color: "white", textAlign: "center", flex: 1 }}>
+                Vous n'avez pas de nouveaux colis !
+              </Text>
+              <TouchableOpacity onPress={handleButtonClick}>
+                <Ionicons
+                  name="arrow-forward-outline"
+                  size={24}
+                  color="white"
+                />
+              </TouchableOpacity>
             </View>
           </View>
-        )}
+          <View
+            style={{
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              paddingTop: 40,
+              paddingBottom: 40,
+              shadowColor: "#000",
+              shadowOffset: { width: 2, height: 2 },
+              shadowOpacity: 0.5,
+              shadowRadius: 4,
+              marginBottom: 10,
+              position: "relative", // Ensure the button is positioned relative to this parent
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={{ color: "white", textAlign: "center", flex: 1 }}>
+                Vous n'avez pas de nouvelles lettres !
+              </Text>
+              <TouchableOpacity onPress={handleButtonClick}>
+                <Ionicons
+                  name="arrow-forward-outline"
+                  size={24}
+                  color="white"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </View>
-    </NativeBaseProvider>
+
+      {/* Footer Bar */}
+      <View
+        style={{
+          height: 50,
+          backgroundColor: "grey",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingHorizontal: 20,
+        }}
+      >
+        <TouchableOpacity onPress={handleButtonClick}>
+          <Image
+            source={require("./assets/icons/gear.png")}
+            style={{ width: 25, height: 25 }}
+          />
+        </TouchableOpacity>
+      </View>
+      {/* Popup */}
+      {popupVisible && (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 20,
+              borderRadius: 8,
+              shadowColor: "#000",
+              shadowOffset: { width: 2, height: 2 },
+              shadowOpacity: 0.5,
+              shadowRadius: 4,
+            }}
+          >
+            <Text>Clicked!</Text>
+          </View>
+        </View>
+      )}
+    </View>
   );
 }
