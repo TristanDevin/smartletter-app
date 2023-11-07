868d597a6630949a8cdf9d73a39fcaa53cb1ec86
TristanDevin <tristan.devin@insa-lyon.fr>
2023-11-07 21:14:06 +0100

    feat: :sparkles: start working on mqtt working

--- components/colisPage.js
+++ components/colisPage.js
@@ -1,21 +1,53 @@
-import { React } from "react";
-import { View, Text, Image } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React, { useEffect } from "react";
+import { View, Text } from "react-native";
 
-export default function ColisPage() {
+
+const mqtt = require("mqtt/dist/mqtt");
+
+const ColisPage = () => {
+  useEffect(() => {
+    // Define your MQTT broker URL and options
+    const brokerUrl = "eu1.cloud.thethings.network"
+    const options = {
+      clientId: "rosaliebdm",
+      username: "smart-letter@ttn",
+      password: "NNSXS.KXBZWVHPB3N62GKK45LXIBPUJ3Y7NOYPZAX2BNY.P77BFQD3FP3YIQMCHE4C557BYMHLOI4YGQ2H6WNEHJJCRR56PFNQ",
+      port: 443,
+      protocolID: "MQIsdp",
+      protocolVersion: 3,
+      keepalive: 60,
+      clean: true,
+      encoding: "utf8",
+    };
+
+    // Connect to the MQTT broker
+    const client = mqtt.connect('wss://eu1.cloud.thethings.network', options);
+
+    // Set up event handlers
+    client.on("connect", () => {
+      console.log("Connected");
+      client.subscribe("smartletter@ttn/+/uplink");
+    });
+
+    client.on("message", (topic, message) => {
+      console.log("New message: ", topic, message.toString());
+    }
+    );
+
+    // Unsubscribe & disconnect after 5 seconds
+    setTimeout(() => {
+      client.unsubscribe("smartletter@ttn/+/uplink");
+      client.end();
+    }, 5000);
+  }
+  );
+
   return (
-    <View style={{ flex: 1, backgroundColor: "black" }}>
-      {/* Top Bar */}
-      <View
-        style={{
-          height: 50,
-          backgroundColor: "grey",
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-        }}
-      >
-        <Text style={{ color: "white" }}>Hello World !</Text>
-      </View>
+    <View>
+      <Text>Your React Native Component</Text>
+      {/* Add UI components to display MQTT messages */}
     </View>
   );
-}
+};
+
+export default ColisPage;
