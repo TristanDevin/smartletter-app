cb3d8dffcad450143054a2a58aa6c5396f0e74d7
Maxence <91471849+MaxenceGueyffier@users.noreply.github.com>
2023-11-23 11:16:38 +0100

    test

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,26 @@
 const express = require("express");
+var request = require('request');
+
 
 const app = express();
 const port = 3000;
+const address = `http://localhost:${port}`
+
+app.route('/data').post(async(req, res) => { 
+  try{
+    console.log(req)
+  } catch(err){
+    console.log(err)
+  }
+});
 
 app.route('/').get((req, res) => {
-  res.send("Hello World !");
+  res.send("Hello World !!");
 });
 app.route('/dev').get((req, res) => {
     res.send("test");    
 });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
