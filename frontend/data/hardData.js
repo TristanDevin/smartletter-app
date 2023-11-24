1cd6130de5f3e3829dcddb1d91efe9b2fee2c275
Rosalie19 <rosalie.biedermann@gmail.com>
2023-11-24 19:03:03 +0100

    pages resume et historique

---
+++ frontend/data/hardData.js
@@ -0,0 +1,29 @@
+const DATA = [
+    {
+
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        letter: 3,
+        colis: 1,
+        date: '11/10/23',
+        time: '15:20',
+        recupere: true,
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        letter: 0,
+        colis: 1,
+        date: '03/10/23',
+        time: '13:50',
+        recupere: false,
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        letter: 1,
+        colis: 0,
+        date: '02/10/23',
+        time: '11:10',
+        recupere: false,
+    },
+];
+
+export default DATA;
