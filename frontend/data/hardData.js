0add63e35b4a0b7704e23ceb06df68cd1ef42235
RosalieBdm <rosalie.biedermann@insa-lyon.fr>
2023-11-24 22:53:51 +0100

    insertion des données avec le bon format (toujours pas de vraie db)

--- frontend/data/hardData.js
+++ frontend/data/hardData.js
@@ -1,28 +1,25 @@
 const DATA = [
     {
 
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        letter: 3,
-        colis: 1,
-        date: '11/10/23',
-        time: '15:20',
-        recupere: true,
+        id: 1,
+        numLetter: 3,
+        numColis: 1,
+        receivedAt: "2023-11-04T14:30:00.000Z",
+        retrieved: false,
     },
     {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        letter: 0,
-        colis: 1,
-        date: '03/10/23',
-        time: '13:50',
-        recupere: false,
+        id: 2,
+        numLetter: 0,
+        numColis: 1,
+        receivedAt: "2023-05-24T08:00:00.000Z",
+        retrieved: false,
     },
     {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        letter: 1,
-        colis: 0,
-        date: '02/10/23',
-        time: '11:10',
-        recupere: false,
+        id: 3,
+        numLetter: 1,
+        numColis: 0,
+        receivedAt: "2023-03-17T10:50:00.000Z",
+        retrieved: false,
     },
 ];
 
