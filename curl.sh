66d49ee11b5aab2cc96e06c1159b44bd921fe860
Maxence <91471849+MaxenceGueyffier@users.noreply.github.com>
2023-11-24 17:02:42 +0100

    post create json

--- curl.sh
+++ curl.sh
@@ -4,20 +4,21 @@ generate_post_data()
 {
   cat <<EOF
 {
-    "login": "$login",
-    "password": "$password",
-    "timestamp": "$timestamp"
+    "id": "$id",
+    "payload": "$payload",
+    "datetime": "$datetime"
 }
 EOF
 }
 
 if [ "$1" == "emit" ]; then
     echo "emit data"
-    login="my_login"
-    password="my_password"
-    timestamp="$(date +%R) $(date +%d/%m/%Y)"
+    id="my_id"
+    payload="312F30"
+    datetime="$(date +%X) $(date +%d/%m/%Y)"
     curl -X POST http://localhost:3000/data -H 'Content-Type: application/json' -d "$(generate_post_data)"
-else
+fi
+if [ "$1" == "get" ]; then
     echo "get data"
     #curl -X POST http://localhost:3000/data -H 'Accept: application/json' -b "type=get"
 fi
