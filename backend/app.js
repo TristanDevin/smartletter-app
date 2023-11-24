432cc051332b87f1184ddb6901a6fe96f1e62a8e
TristanDevin <tristan.devin@insa-lyon.fr>
2023-11-24 17:09:00 +0100

    add db

--- backend/app.js
+++ backend/app.js
@@ -19,23 +19,47 @@ const pool = new Pool({
 
 
 const app = express();  
-app.use(express.json())
+app.use(express.json());
 const port = 3000;
 const address = `http://localhost:${port}/api`
 
+function hexToAscii(hexString) {
+  let asciiString = '';
+  
+  for (let i = 0; i < hexString.length; i += 2) {
+      let hex = hexString.substr(i, 2);
+      let decimal = parseInt(hex, 16);
+      asciiString += String.fromCharCode(decimal);
+  }
+  
+  return asciiString;
+}
+
 app.route('/data').post(async(req, res) => { 
   try{
-    console.log(req.body)
+    var json = {"id":req.body.id};
+    let letter_colis_string = hexToAscii(req.body.payload);
+    letter_colis = letter_colis_string.split('/');
+    json.letter = letter_colis[0];
+    json.colis = letter_colis[1];
+    json.datetime = req.body.datetime;
+    json.recupere = false;
+    console.log(json);
+
+    var json = JSON.stringify(json);
+    fs.writeFile('test.json', json, 'utf8', function(err) {
+      if (err) console.log(err);
+    });
   } catch(err){
-    console.log(err)
+    console.log(err);
   }
 });
 
 app.route('/messages').get(async(req, res) => { 
   try{
-    console.log(req.body)
+    console.log(req.body);
   } catch(err){
-    console.log(err)
+    console.log(err);
   }
 });
 
