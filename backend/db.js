65815db3b82621faf4141f51212e2667c61cf1bb
TristanDevin <tristan.devin@insa-lyon.fr>
2023-11-27 16:12:11 +0100

    remove useless stuff

--- backend/db.js
+++ backend/db.js
@@ -1,46 +1,5 @@
-<<<<<<< HEAD
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-
-async function getMessages() {
-  return prisma.message.findMany();
-}
-
-async function postMessage(message) {
-  return prisma.message.create({
-    data: {
-      senderDevice: message.senderDevice,
-      numLetter: message.numLetter,
-      numColis: message.numColis,
-      receivedAt: message.receivedAt,
-      retrieved: message.retrieved,
-    },
-  });
-}
-
-async function putMessage(message) {
-  return prisma.message.update({
-    where: { id: message.id },
-    data: {
-      retrieved: message.retrieved,
-    },
-  });
-}
-
-module.exports = { getMessages, postMessage };
-=======
-const { Pool } = require("pg");
-require("dotenv").config(); // Load environment variables from .env
-
-
-
-const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASS,
-  port: process.env.DATABASE_PORT,
-});
 
 async function getMessages() {
   return prisma.message.findMany();
